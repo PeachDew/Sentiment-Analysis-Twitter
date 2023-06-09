225ccc8d86ef35ae25ccb3e120cbc6031a7b61ab
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-09 23:14:41 +0800

    Debugging streamlit

--- streamlit/.ipynb_checkpoints/streamlit_app-checkpoint.py
+++ streamlit/.ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -1,16 +1,8 @@
 import streamlit as st
 import pickle
-import os
 
-def file_selector(folder_path='.'):
-    filenames = os.listdir(folder_path)
-    selected_filename = st.selectbox('Select a file', filenames)
-    return os.path.join(folder_path, selected_filename)
 
-filename = file_selector()
-st.write('You selected `%s`' % filename)
-
-with open(filename, 'rb') as f:
+with open('./streamlit/sample_data.pickle', 'rb') as f:
     df = pickle.load(f)
 
 print(df.head())
