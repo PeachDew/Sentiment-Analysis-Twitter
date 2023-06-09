4e4a64a9264d8689096d47d8dbc28f1bb4bd8eb2
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-09 23:10:20 +0800

    Debugging streamlit

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,12 +1,8 @@
 import streamlit as st
 import pickle
-import os
 
-absolute_path = os.getcwd()
-data_pickle = 'sample_data.pickle'
-file_path = absolute_path + '/' + data_pickle
 
-with open(file_path, 'rb') as f:
+with open('sentiment-analysis-twitter/sample_data.pickle', 'rb') as f:
     df = pickle.load(f)
 
 print(df.head())
