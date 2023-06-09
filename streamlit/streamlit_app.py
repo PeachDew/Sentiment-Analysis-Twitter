f13e597da654e79e609b8050e914da86b872e5c9
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-09 23:03:21 +0800

    Debugging streamlit

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -1,7 +1,12 @@
 import streamlit as st
 import pickle
+import os
 
-with open('sample_data.pickle', 'rb') as f:
+absolute_path = os.getcwd()
+data_pickle = 'sample_data.pickle'
+file_path = absolute_path + '/' + data_pickle
+
+with open(file_path, 'rb') as f:
     df = pickle.load(f)
 
-df.head()
+print(df.head())
