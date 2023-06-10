3333d39729a1ce64e22a1385073fba8559ee44c2
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-11 02:03:05 +0800

    Testing tweet predictions

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -4,10 +4,51 @@ import pickle
 
 with open('./streamlit/sample_data.pickle', 'rb') as f:
     df = pickle.load(f)
+    
+with open('./streamlit/tfidf_logreg_model.pickle', 'rb') as f:
+    best_logreg_model = pickle.load(f)
+    
+with open('./streamlit/vectorizer.pickle', 'rb') as f:
+    vectorizer = pickle.load(f)
 
 st.title('Twitter Sentiment Analysis')
 st.text('Sample of dataframe:')
 st.dataframe(df.head(10))
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+import string
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+
+lemmatizer = WordNetLemmatizer()
+
+def preprocess_text(text):
+    # remove punctuations
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    # tokenize text
+    tokens = word_tokenize(text)
+    # removing stopwords
+    stop_words = set(stopwords.words('english'))
+    tokens = [token for token in tokens if token.lower() not in stop_words]
+    # lemmatize tokens
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    
+    return tokens
+
+def predict_tweet_probability(tweet, model):
+    cleaned_tweet = preprocess_text(tweet)
+    print(cleaned_tweet)
+    cleaned_tweet = ' '.join(cleaned_tweet)
+    print(cleaned_tweet)
+    transformed_tweet = vectorizer.transform([cleaned_tweet])
+    # Predict the probability of the tweet being positive
+    probability = model.predict_proba(transformed_tweet)[:, 1]
+    return probability[0]
 
 col1, col2 = st.columns(2)
 
@@ -21,4 +62,9 @@ with col2:
     )
 
     if text_input:
-        st.write("You entered: ", text_input)
+        probability = predict_tweet_probability(tweet, best_logreg_model)
+
+        if probability > 0.5:
+            print(f"Probability that the tweet is positive: {probability * 100:.3g}%")
+        else:
+            print(f"Probability that the tweet is negative: {(1 - probability) * 100:.3g}%")
