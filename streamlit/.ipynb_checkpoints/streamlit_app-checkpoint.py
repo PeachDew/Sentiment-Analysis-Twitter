82e86200683e56098736ce4a3e032c1865af1e76
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-11 02:22:36 +0800

    Debugging Predictions

--- streamlit/.ipynb_checkpoints/streamlit_app-checkpoint.py
+++ streamlit/.ipynb_checkpoints/streamlit_app-checkpoint.py
@@ -63,7 +63,7 @@ with col2:
     )
 
     if text_input:
-        probability = predict_tweet_probability(tweet, best_logreg_model)
+        probability = predict_tweet_probability(text_input, best_logreg_model)
 
         if probability > 0.5:
             print(f"Probability that the tweet is positive: {probability * 100:.3g}%")
