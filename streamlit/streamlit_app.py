52f9cc3fb93914634e4ee44743b865c6254fe843
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-11 00:39:18 +0800

    Testing input/output

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -6,4 +6,17 @@ with open('./streamlit/sample_data.pickle', 'rb') as f:
     df = pickle.load(f)
 
 st.title('Twitter Sentiment Analysis')
+st.text('Sample of dataframe:')
 st.dataframe(df.head(10))
+
+with col1:
+    st.title('Tweet Prediction')
+
+with col2:
+    text_input = st.text_input(
+        "Tweet Away 👇",
+        placeholder="Enter Tweet Here",
+    )
+
+    if text_input:
+        st.write("You entered: ", text_input)
