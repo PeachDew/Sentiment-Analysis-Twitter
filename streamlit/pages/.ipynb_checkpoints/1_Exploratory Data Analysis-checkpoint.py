a6538f2a83e16b0e7173b8004d435cbb8beedc81
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-11 23:13:09 +0800

    Trying Multi Pages

--- streamlit/pages/.ipynb_checkpoints/1_Exploratory Data Analysis-checkpoint.py
+++ streamlit/pages/.ipynb_checkpoints/1_Exploratory Data Analysis-checkpoint.py
@@ -29,7 +29,12 @@ with tab1:
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
     
 with tab2:
-    fig = px.histogram(df, x="day")
+    color_discrete_map = {}
+    for i in range(1, 32):
+        darkness = i / 31  # Gradually darkening value from 0 to 1
+        color = f"#{darkness:02x}{darkness:02x}{darkness:02x}"  # Hex color with gradually increasing darkness
+        color_discrete_map[i] = color
+    fig = px.histogram(df, x="day", color="day", color_discrete_map=color_discrete_map)
     st.plotly_chart(fig, theme="streamlit", use_container_width=True)
     
 with tab3:
