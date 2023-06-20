deb204bc9b43e84c85605158f7f1cc7bd8e24cd7
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-20 23:28:52 +0800

    New page, gender classification

--- streamlit/pages/2_Gender_Prediction.py
+++ streamlit/pages/2_Gender_Prediction.py
@@ -17,24 +17,30 @@ st.write(
 with open('./streamlit/sample_data.pickle', 'rb') as f:
     df = pickle.load(f)
 
+col1, col2 = st.columns(2)
+with col1:
+    title = st.text_input('Twitter Username', 'Lydia23')
+    st.write('Your username is:', title)
 
-title = st.text_input('Twitter Username', 'Lydia23')
-st.write('Your username is:', title)
+    favno = st.number_input('Favorite number', value=42)
+    st.write('Your favorite number is:', favno)
 
-favno = st.number_input('Favorite number', value=42)
-st.write('Your favorite number is:', favno)
+    tweets = st.number_input('Number of tweets', value=500)
+    st.write('You tweeted this many times:', tweets)
 
-tweets = st.number_input('Number of tweets', value=500)
-st.write('You tweeted this many times:', tweets)
+    desc = st.text_area('Twitter description', 'I love farming!')
+    st.write("Your description:", desc)
 
-desc = st.text_area('Twitter description', 'I love farming!')
-st.write("Your description:", desc)
+    txt = st.text_area('Paste a random tweet from your account:', 'Feelin good at the sunny beach B)')
+    st.write("Your tweet:", txt)
 
-txt = st.text_area('Paste a random tweet from your account:', 'Feelin good at the sunny beach B)')
-st.write("Your tweet:", txt)
-
-color = st.color_picker('Your twitter link color', '#1DA1F2')
-st.write('The current color is', color)
-
+    color = st.color_picker('Your twitter link color', '#1DA1F2')
+    st.write('The current color is', color)
+    
+    pred_button = st.button('Generate')
+    
+with col2:
+    if pred_button:
+        st.write("Prediction here")
 
 
