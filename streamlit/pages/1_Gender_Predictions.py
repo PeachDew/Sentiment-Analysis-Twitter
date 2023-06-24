8437f912c835a0c98b172e94a0480d77919e3349
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-25 06:16:40 +0800

    Debugging Predictions

--- streamlit/pages/1_Gender_Predictions.py
+++ streamlit/pages/1_Gender_Predictions.py
@@ -245,7 +245,7 @@ with color2:
             r, g, b = hex_to_rgb(color)
             y = np.array([r,g,b])
             mycolors = ["red", "green", "blue"]
-            fig, ax = plt.subplots(figsize=(0.3, 0.3))
+            fig, ax = plt.subplots(figsize=(1, 1))
             ax.grid(False)
             plt.pie(y, colors=mycolors)
             fig.set_facecolor("none")
