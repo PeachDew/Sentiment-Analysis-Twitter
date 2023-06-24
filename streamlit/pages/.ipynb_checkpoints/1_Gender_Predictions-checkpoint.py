28b9261d0d69ea806170cdf361611aedea8d2a55
PeachDew <chin.furui.raymond@dhs.sg>
2023-06-25 05:42:53 +0800

    Debugging Predictions

--- streamlit/pages/.ipynb_checkpoints/1_Gender_Predictions-checkpoint.py
+++ streamlit/pages/.ipynb_checkpoints/1_Gender_Predictions-checkpoint.py
@@ -252,7 +252,8 @@ with color2:
                 y = np.array([r,g,b])
                 mylabels = ["Red", "Green", "Blue"]
                 mycolors = ["red", "green", "blue"]
-                plt.pie(y, labels = mylabels, colors = mycolors, wedgeprops={'alpha':0})
+                plt.pie(y, labels = mylabels, colors = mycolors)
+                fig.set_facecolor('lightgrey',alpha=0)
                 st.pyplot(plt)    
             else:
                 st.error("Please fill in all the input fields.")
